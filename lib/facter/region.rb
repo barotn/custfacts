8671d136601947f671482f8756b9cc3a2a29aabc
Nilesh Barot <barotnilesh@Nileshs-MacBook-Pro.local>
2017-04-28 09:47:58 +0100

    add new fact

---
+++ lib/facter/region.rb
@@ -0,0 +1,13 @@
+Facter.add(:region) do
+  setcode do
+    custregion = Facter.value('hostname').to_s
+    region = custregion.split('-')[0][2,2]
+    if region == 'we'
+      'eu-west'
+    elsif region == 'ce'
+      'eu-central'
+    elsif
+      'region not specified'
+    end
+  end
+end 
